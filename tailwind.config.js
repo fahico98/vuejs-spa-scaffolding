4eb2d4db3ad38c123ac730bd4fd6f1349e5111d8
Fahibram Cárcamo <fahico98@gmail.com>
2023-11-09 01:58:53 -0500

    style(auth): Assets and styles for authentication system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const defaultTheme = require("tailwindcss/defaultTheme")
 
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    // If you want to add additional small breakpoints, you can’t use "extend" because the
+    // small breakpoint would be added to the end of the breakpoint list, and breakpoints
+    // need to be sorted from smallest to largest in order to work as expected with a
+    // min-width breakpoint system.
+    screens: {
+      xs: "400px", // @media (min-width: 400px) { ... }
+      ...defaultTheme.screens
+    },
+
+    fontFamily: {
+      sans: ["Rubik", ...defaultTheme.fontFamily.sans], // Texto base (By default)
+      serif: ["Poppins", ...defaultTheme.fontFamily.serif], // Títulos y subtitulos
+      mono: ["Red Hat Mono", ...defaultTheme.fontFamily.mono] // Enlaces y botones
+    },
+
+    extend: {
+      colors: {
+        "my-yellow": "#FFCD05",
+        "my-yellow-dark": "#C69E00"
+      }
+    }
+  },
+
+  plugins: [require("@tailwindcss/forms")]
+}
