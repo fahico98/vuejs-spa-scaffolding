41f9abf82851248e468778de1178fc0dc56fab0e
Fahibram Cárcamo <fahico98@gmail.com>
2023-11-09 01:54:43 -0500

    config: Code linting and formatting packages added
    ESLint, Prettier and Commitlint packages setted up.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "url"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,13 @@ export default defineConfig({
       usePolling: true
     }
   },
-  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url))
+      }
+    ]
+  },
+  plugins: [vue()]
 })
